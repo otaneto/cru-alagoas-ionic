e431256ace23617c7b0fca062e8113f85bc4364c
Otacílio Neto <otaneto@rogalabs.com>
2018-06-14 23:51:17 -0300

    Event and Show Event pages are working with static data

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ShowEventPage } from '../show-event/show-event';
 
 @Component({
   selector: 'page-events',
   templateUrl: 'events.html'
 })
 export class EventsPage {
+  events: any[];
+  eventsFound: any[];
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ngOnInit() {
+    this.initEventsArray();
   }
 
+  initEventsArray() {
+    this.events = [
+      { name: 'AGORA 2018' },
+      { name: 'Encontrão Café com a CRU' }
+    ];
+    this.eventsFound = this.events;
+  }
+
+  getEvents(event: any) {
+    const value = event.target.value;
+
+    const expression = new RegExp(value, 'i');
+    this.eventsFound = this.events.filter(item => expression.test(item.name));
+  }
+
+  showEvent(event: any) {
+    this.navCtrl.push(ShowEventPage, { ...event });
+  }
 }
