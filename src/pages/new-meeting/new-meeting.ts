d15dfe8063f69107bce851539b6418ecd5156628
otaneto <otaciliolucas@gmail.com>
2018-07-16 14:06:20 -0300

    Map in progress. Needs fixes on transition between modal and page

--- src/pages/new-meeting/new-meeting.ts
+++ src/pages/new-meeting/new-meeting.ts
@@ -6,6 +6,8 @@ import {
   ModalController
 } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import leaflet from 'leaflet';
+
 import { SelectLocationPage } from '../select-location/select-location';
 
 @IonicPage()
@@ -17,6 +19,9 @@ export class NewMeetingPage implements OnInit {
   meetingForm: FormGroup;
   days: any[] = [];
   time: any;
+  myMap: any;
+  center: leaflet.PointTuple;
+  marker: any;
 
   constructor(
     private modalCtrl: ModalController,
@@ -27,12 +32,14 @@ export class NewMeetingPage implements OnInit {
 
   ngOnInit() {
     this.initMeetingForm();
+    this.center = ['-9.6475', '-35.7337'];
+    this.loadMap();
   }
 
   initMeetingForm() {
     this.meetingForm = this.fb.group({
       name: ['', Validators.required],
-      geolocation: ['', Validators.required],
+      location: ['', Validators.required],
       days: [[''], Validators.required],
       time: ['', Validators.required],
       description: ['', Validators.required],
@@ -57,7 +64,43 @@ export class NewMeetingPage implements OnInit {
   }
 
   onOpenMap() {
+    this.myMap.remove();
     const modal = this.modalCtrl.create(SelectLocationPage);
     modal.present();
+    modal.onDidDismiss(data => {
+      if (data) {
+        this.meetingForm.patchValue({
+          location: data.location
+        });
+        this.center = data.location;
+        this.loadMap();
+      }
+    });
+  }
+
+  loadMap() {
+    this.myMap = leaflet.map('map').setView(this.center, 13);
+
+    leaflet
+      .tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17
+      })
+      .addTo(this.myMap);
+
+    this.markPlace();
+
+    // marker.bindPopup('<p> Leaflet Mapa Funcionando. </p>');
+  }
+
+  markPlace() {
+    if (this.marker) {
+      this.myMap.removeLayer(this.marker);
+    }
+    this.marker = leaflet.marker(this.center);
+    this.myMap.addLayer(this.marker);
+  }
+
+  ionViewWillLeave() {
+    this.myMap.remove();
   }
 }
--- src/pages/select-location/select-location.ts
+++ src/pages/select-location/select-location.ts
@@ -1,8 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ViewController } from 'ionic-angular';
 import { NominatimJS } from 'nominatim-js';
 import leaflet from 'leaflet';
-import { dropRight } from 'lodash';
 
 @IonicPage()
 @Component({
@@ -14,9 +13,10 @@ export class SelectLocationPage {
   myMap: any;
   center: leaflet.PointTuple;
   marker: any;
-  @ViewChild('map') mapContainer: ElementRef;
+  place: any;
+  @ViewChild('map-select') mapContainer: ElementRef;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public viewCtrl: ViewController) {}
 
   ionViewDidEnter() {
     this.center = ['-9.6475', '-35.7337'];
@@ -24,7 +24,7 @@ export class SelectLocationPage {
   }
 
   loadMap() {
-    this.myMap = leaflet.map('map').setView(this.center, 13);
+    this.myMap = leaflet.map('selecet-map').setView(this.center, 13);
 
     leaflet
       .tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -46,21 +46,30 @@ export class SelectLocationPage {
   }
 
   searchPlace(event) {
-    if (event.keyCode === 13 || event.type === 'blur') {
-      NominatimJS.search({
-        q: event.target.value
+    NominatimJS.search({
+      q: event.target.value
+    })
+      .then(data => {
+        this.placesFound = data;
       })
-        .then(data => {
-          this.placesFound = data;
-        })
-        .catch(error => console.log(error));
-    }
+      .catch(error => console.log(error));
   }
 
   selectPlace(place) {
+    this.place = place;
     leaflet.layerGroup().addTo(this.myMap);
     this.center = [place.lat, place.lon];
     this.myMap.setView(this.center, 15);
     this.markPlace();
+  }
+
+  confirmPlace() {
+    this.viewCtrl.dismiss({ location: this.place });
+    this.myMap.remove();
+  }
+
+  abort() {
+    this.viewCtrl.dismiss();
+    this.myMap.remove();
   }
 }
